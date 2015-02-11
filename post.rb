6192b2344fa3916bb97d777c916adad55e51a956
drewhgood <drewhgood@gmail.com>
2015-02-11 19:03:42 +0000

    Initial commit.

---
+++ post.rb
@@ -0,0 +1,12 @@
+class Post
+
+  attr_accessor :title, :url, :points, :item_id
+
+  def initialize(title, url, points, item_id)
+    @title = title
+    @url = url
+    @points = points
+    @item_id = item_id
+  end
+
+end
